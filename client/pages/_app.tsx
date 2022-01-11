91b7b65e68ffada2bc56d997bbacbf4baa591065
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2022-01-11 10:02:06 -0500

    Add website title and favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,10 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { TransactionProvider } from '../context/TransactionContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
 		<>
+			<Head>
+				<title>Kryptfer</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<link rel='shortcut icon' type='image/ico' href='/favicon.ico' />
+			</Head>
 			<TransactionProvider>
 				<Layout>
 					<Component {...pageProps} />
